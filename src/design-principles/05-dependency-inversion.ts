06698a57651a6b85e8e6a645bfdf5e60e6230584
shljsl75891 <shljsl75891@gmail.com>
2025-04-19 00:49:10 +0530

    feat: dependency-inversion principle

---
+++ src/design-principles/05-dependency-inversion.ts
@@ -0,0 +1,75 @@
+// --------------------------------------COMPLIANT CODE---------------------------
+// Low Level Module
+class MySqlDatabase {
+  save(data: string): void {
+    console.log(`Saving ${data} to MySQL database`);
+  }
+}
+
+class _HighLevelModule {
+  constructor(private database: MySqlDatabase) {}
+
+  execute(data: string) {
+    this.database.save(data);
+  }
+}
+
+const mysql = new MySqlDatabase();
+const highLvl = new _HighLevelModule(mysql);
+
+highLvl.execute("Compliant Code");
+
+// ----------------------------------------------------------------------------------------
+
+/**
+ * NON COMPLIANT CODE - Dependency Inversion Principle
+ *
+ * The high level module must not depend on low level modules directly. Instead
+ * they should depend on abstractions (interfaces / abstract classes).
+ *
+ * Abstractions shouldn't depend on details (underlying implementation),
+ * instead it should be dependent on abstractions. Low Level Modules
+ * must implement contract declared by interfaces / abstract classes.
+ */
+interface IDatabase {
+  save(data: string): void;
+}
+
+// Low Level Modules
+class PostgresDatabase implements IDatabase {
+  save(data: string): void {
+    console.log(`Saving ${data} to PostgreSQL database`);
+  }
+}
+
+class MongoDatabase implements IDatabase {
+  save(data: string): void {
+    console.log(`Saving ${data} to Mongo database`);
+  }
+}
+
+class RedisDatabase implements IDatabase {
+  save(data: string): void {
+    console.log(`Saving ${data} to Redis database`);
+  }
+}
+
+class HighLevelModule {
+  constructor(private database: IDatabase) {}
+
+  execute(data: string) {
+    this.database.save(data);
+  }
+}
+
+const pg = new PostgresDatabase();
+const redis = new RedisDatabase();
+const mongo = new MongoDatabase();
+
+const user = new HighLevelModule(mongo);
+const auth = new HighLevelModule(redis);
+const post = new HighLevelModule(pg);
+
+user.execute("User data");
+auth.execute("Auth data");
+post.execute("Post data");
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import "./design-principles/01-single-responsibility";
+// import "./design-principles/01-single-responsibility";
 // import "./design-principles/02-open-closed";
 // import "./design-principles/03-liskov-substituion";
 // import "./design-principles/04-interface-segregation";
-// import "./design-principles/05-dependency-inversion";
+import "./design-principles/05-dependency-inversion";
